b1f0a9e4ce481bc67a9d7776d431e002820b12bc
GabrielReds13 <speedckprod4007@gmail.com>
2024-10-18 22:22:08 -0300

    Fix: Tailwind Library Fixed

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-	import { VContainer } from "vuetify/components";
 </script>
 
 <template>
-	<div>
-		<VAppBar> My App </VAppBar>
-		<VNavigationDrawer></VNavigationDrawer>
-		<VContainer>
-			<h2>Dashboar</h2>
-			<p>
-				Welcome to your dashboard. You can add more pages to your app by
-				creating new files in the pages directory
-			</p>
-		</VContainer>
+	<div class="grid grid-cols-2 w-full h-full">
+		<v-card text="..."></v-card>
+
+		<!-- adicionar -->
+		<div class="bg-red-500 w-full h-full grid-span"></div>
 	</div>
 </template>
