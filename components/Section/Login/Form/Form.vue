d45d17b82477066a403982ccc782bdcecdab2741
Kauan Marques <daluzmarkes@gmail.com>
2024-10-23 18:54:23 -0400

    refactor: Organized folders

---
+++ components/Section/Login/Form/Form.vue
@@ -0,0 +1,22 @@
+<template>
+	<v-form class="flex flex-col justify-center w-2/3 p-8 gap-5">
+		<input
+			placeholder="Email"
+			class="h-14 rounded-lg border-2 border-solid border-[#4F5978] text-white p-4"
+		/>
+		<input
+			placeholder="Senha"
+			class="h-14 rounded-lg border-2 border-solid border-[#4F5978] text-white p-4"
+		/>
+		<input
+			type="button"
+			value="Entrar"
+			class="h-14 w-full rounded-lg bg-white text-[#4F5978]"
+			src=""
+		/>
+	</v-form>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
