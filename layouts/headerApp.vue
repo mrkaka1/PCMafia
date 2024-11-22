834fcb1e25a46ff8cb3b247ce2dc51dede7a6a99
Kauan Marques <daluzmarkes@gmail.com>
2024-11-22 15:12:52 -0400

    fix: created appBar

--- layouts/headerApp.vue
+++ layouts/headerApp.vue
@@ -1,18 +1,27 @@
 <template>
 	<div>
-		<v-app-bar :elevation="2" color="#0C0C0C" class="h-[75px]">
+		<v-app-bar :elevation="2" color="#0C0C0C" class="h-[85px]">
 			<v-app-bar-title>
-				<v-form>
-					<input
+				<v-form class="mt-4">
+					<SearchBar />
+					<!-- <input
 						placeholder="Buscar..."
-						class="h-14 rounded-lg border-2 border-solid border-[#4F5978] text-white p-4"
-					/>
+						class="h-12 rounded-lg border-2 border-solid border-[#4F5978] text-white p-4 mt-3"
+					/> -->
 				</v-form>
 			</v-app-bar-title>
+			<div class="flex flex-row gap-6 mr-8 mt-5">
+				<nuxt-link to="./">
+					<IconsIconNewItem />
+				</nuxt-link>
+				<nuxt-link to="">
+					<IconsIconHistorical />
+				</nuxt-link>
+				<nuxt-link to="./">
+					<IconsIconLogout />
+				</nuxt-link>
+			</div>
 		</v-app-bar>
-    <div>
-      
-    </div>
 
 		<div>
 			<slot />
@@ -20,6 +29,8 @@
 	</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+	import SearchBar from "~/components/VV/SearchBar.vue";
+</script>
 
 <style></style>
